2cfb6a1401c82fabf3e7cd0a0c020d960b42d778
Pedro Rodrigues <pir.pedro@gmail.com>
2021-03-24 10:54:05 -0300

    First commit

---
+++ frontend/.docker/entrypoint.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+npm install
+/bin/sh -c "while sleep 1000; do :; done"
---
+++ frontend/scripts/start.js
@@ -0,0 +1,76 @@
+'use strict';
+
+// Do this as the first thing so that any code reading it knows the right env.
+process.env.BABEL_ENV = 'development';
+process.env.NODE_ENV = 'development';
+
+// Makes the script crash on unhandled rejections instead of silently
+// ignoring them. In the future, promise rejections that are not handled will
+// terminate the Node.js process with a non-zero exit code.
+process.on('unhandledRejection', err => {
+  throw err;
+});
+
+// Ensure environment variables are read.
+require('../config/env');
+
+
+const fs = require('fs');
+const chalk = require('react-dev-utils/chalk');
+const webpack = require('webpack');
+const checkRequiredFiles = require('react-dev-utils/checkRequiredFiles');
+const {
+  createCompiler,
+} = require('../config/WebpackDevServerUtils');
+const paths = require('../config/paths');
+const configFactory = require('../config/webpack.config');
+
+const useYarn = fs.existsSync(paths.yarnLockFile);
+
+// Warn and crash if required files are missing
+if (!checkRequiredFiles([paths.appHtml, paths.appIndexJs])) {
+  process.exit(1);
+}
+
+if (process.env.HOST) {
+  console.log(
+    chalk.cyan(
+      `Attempting to bind to HOST environment variable: ${chalk.yellow(
+        chalk.bold(process.env.HOST)
+      )}`
+    )
+  );
+  console.log(
+    `If this was unintentional, check that you haven't mistakenly set it in your shell.`
+  );
+  console.log(
+    `Learn more here: ${chalk.yellow('https://cra.link/advanced-config')}`
+  );
+  console.log();
+}
+
+// We require that you explicitly set browsers and do not fall back to
+// browserslist defaults.
+
+const config = configFactory('development');
+const appName = require(paths.appPackageJson).name;
+const useTypeScript = fs.existsSync(paths.appTsConfig);
+const tscCompileOnError = process.env.TSC_COMPILE_ON_ERROR === 'true';
+    
+// Create a webpack compiler that is configured with custom messages.
+createCompiler({
+  appName,
+  config,
+  devSocket: {errors: () => {}, warnings: () => {}},
+  urls: {
+    localUrlForTerminal: 'Only Compiled'
+  },
+  useYarn,
+  useTypeScript,
+  tscCompileOnError,
+  webpack,
+}).watch({}, (err, stats) => {
+  if(err){
+    console.error(err);
+  }
+});
